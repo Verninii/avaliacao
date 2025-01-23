1ff12381d332ea8bcd5440eb438760c136c1ed11
Verninii <lorenzzo.vernini@gmail.com>
2025-01-23 13:19:03 -0300

    testandoooooooooooooooooooooooooooooooooooooooooooooo

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())               // instalar a biblioteca do cors permite que qualquer um acesse o backend
 const corsOption = {
-    origin: 'https://guileless-platypus-00484d.netlify.app',
+    origin: 'avaliacao-production.up.railway.app',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }
